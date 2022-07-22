62d027626daee22bd31f99d70cb80aed04c932ce
istarter <najibkhan.bahrian@gmail.com>
2022-07-22 20:19:19 +0500

    Search exercise added

--- src/components/SearchExercise.js
+++ src/components/SearchExercise.js
@@ -2,46 +2,46 @@ import React, { useEffect, useState } from "react";
 import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 
 import { exerciseOptions, fetchData } from "../utils/fetchData";
-import HorizontalScrollbar from "./HorizontalScrollbar";
+// import HorizontalScrollbar from "./HorizontalScrollbar";
 
 const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
   const [search, setSearch] = useState("");
-  const [bodyParts, setBodyParts] = useState([]);
+  // const [bodyParts, setBodyParts] = useState([]);
 
-  useEffect(() => {
-    const fetchExercisesData = async () => {
-      const bodyPartsData = await fetchData(
-        "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
-        exerciseOptions
-      );
+  // useEffect(() => {
+  //   const fetchExercisesData = async () => {
+  //     const bodyPartsData = await fetchData(
+  //       "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
+  //       exerciseOptions
+  //     );
 
-      setBodyParts(["all", ...bodyPartsData]);
-    };
+  //     setBodyParts(["all", ...bodyPartsData]);
+  //   };
 
-    fetchExercisesData();
-  }, []);
+  //   fetchExercisesData();
+  // }, []);
 
-  const handleSearch = async () => {
-    if (search) {
-      const exercisesData = await fetchData(
-        "https://exercisedb.p.rapidapi.com/exercises",
-        exerciseOptions
-      );
+  // const handleSearch = async () => {
+  //   if (search) {
+  //     const exercisesData = await fetchData(
+  //       "https://exercisedb.p.rapidapi.com/exercises",
+  //       exerciseOptions
+  //     );
 
-      const searchedExercises = exercisesData.filter(
-        (item) =>
-          item.name.toLowerCase().includes(search) ||
-          item.target.toLowerCase().includes(search) ||
-          item.equipment.toLowerCase().includes(search) ||
-          item.bodyPart.toLowerCase().includes(search)
-      );
+  //     const searchedExercises = exercisesData.filter(
+  //       (item) =>
+  //         item.name.toLowerCase().includes(search) ||
+  //         item.target.toLowerCase().includes(search) ||
+  //         item.equipment.toLowerCase().includes(search) ||
+  //         item.bodyPart.toLowerCase().includes(search)
+  //     );
 
-      window.scrollTo({ top: 1800, left: 100, behavior: "smooth" });
+  //     window.scrollTo({ top: 1800, left: 100, behavior: "smooth" });
 
-      setSearch("");
-      setExercises(searchedExercises);
-    }
-  };
+  //     setSearch("");
+  //     setExercises(searchedExercises);
+  //   }
+  // };
 
   return (
     <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
@@ -79,19 +79,19 @@ const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
             right: "0px",
             fontSize: { lg: "20px", xs: "14px" },
           }}
-          onClick={handleSearch}
+          // onClick={handleSearch}
         >
           Search
         </Button>
       </Box>
-      <Box sx={{ position: "relative", width: "100%", p: "20px" }}>
+      {/* <Box sx={{ position: "relative", width: "100%", p: "20px" }}>
         <HorizontalScrollbar
           data={bodyParts}
           bodyParts
           setBodyPart={setBodyPart}
           bodyPart={bodyPart}
         />
-      </Box>
+      </Box> */}
     </Stack>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,14 +2,14 @@ import React from "react";
 import { Box } from "@mui/material";
 import HeroBanner from "../components/HeroBanner";
 import SearchExercise from "../components/SearchExercise";
-import Exercises from "../components/Exercises";
+// import Exercises from "../components/Exercises";
 
 const Home = () => {
   return (
     <Box>
       <HeroBanner />
       <SearchExercise />
-      <Exercises />
+      {/* <Exercises />  */}
     </Box>
   );
 };
